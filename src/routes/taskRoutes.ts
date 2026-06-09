0dfcad42783eaef685288ebf48bde848356f38b8
samchat2561 <samchat2561@gmail.com>
2026-06-09 13:55:38 +0700

    update

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -2,6 +2,7 @@ import { Router,Request, Response } from 'express';
 
 const router = Router();
 
+// GET: http://localhost:3000/api/tasks
 router.get('/tasks', (req: Request, res: Response) => {
     res.status(200).json({ message: 'Get all tasks' });
 });
