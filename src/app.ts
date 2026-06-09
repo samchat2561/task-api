b3e7245b808f172e9363a8dc49af61ef31ae0ee9
samchat2561 <samchat2561@gmail.com>
2026-06-09 15:30:04 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ const app: Application = express();
 app.use(express.json());            // Middleware to parse JSON bodies
 app.use('/api', taskRoutes);      // Use task routes with '/api' prefix
 
-//GET: http://localhost:3000/
+//GET: http://localhost:8080/
 app.get('/', (req: Request, res: Response): void => {
     res.status(200).json({ message: 'Hello, World!' });
 });
 
-const PORT: number = 3000;
+const PORT: number = 8080;
 app.listen(PORT, (): void => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,14 +1,16 @@
-// Copyright 2026 patipan
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+import { Router, Request, Response } from 'express';
+import axios from 'axios';
 
+const router = Router();
+const JSON_SERVER_URL = 'https://localhost:3000/tasks';
+
+// Example API route
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const response = await axios.get(`${JSON_SERVER_URL}`);
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+});
+export default router;
