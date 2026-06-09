68524fbd9b2d65ebdb16c70b3fc8d18f77779f68
samchat2561 <samchat2561@gmail.com>
2026-06-09 13:51:08 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express'; 
+import taskRoutes from './routes/taskRoutes';
 
 const app: Application = express();
+app.use(express.json());            // Middleware to parse JSON bodies
+app.use('/api', taskRoutes);      // Use task routes with '/api' prefix
 
 //GET: http://localhost:3000/
 app.get('/', (req: Request, res: Response): void => {
@@ -11,5 +14,3 @@ const PORT: number = 3000;
 app.listen(PORT, (): void => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-    
