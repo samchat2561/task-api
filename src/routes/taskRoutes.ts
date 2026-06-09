2850456a891af2a5726f7f31a0cd191bb46c147b
samchat2561 <samchat2561@gmail.com>
2026-06-09 14:27:38 +0700

    upadte

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,10 +1,21 @@
 import { Router, Request, Response } from 'express';
-import { tasks } from '../data';
+import { Task, tasks } from '../data';
 const router = Router();
 
 // GET: http://localhost:3000/api/tasks
 router.get('/tasks', (req: Request, res: Response) => {
     res.status(200).json({ data: tasks });
+});
+
+// POST: http://localhost:3000/api/tasks
+router.post('/tasks', (req: Request, res: Response) => {
+    const maxId = tasks.length > 0 ? Math.max(...tasks.map(task => task.id)) : 0;
+    const newTask: Task = {
+        id: maxId + 1,
+        ...req.body
+    }
+    tasks.push(newTask);
+    res.status(201).json({ data: newTask });
 });
 
 export default router;
